b1cc7dd7b8d42e97e2d38c4d3f7d95f6322c7a3b
Gulsah Tosun <gulsahustatosun@gmail.com>
2022-12-08 22:24:30 +0000

    Fixing data name

--- src/NewBranch/NewBranch1.java
+++ src/NewBranch/NewBranch1.java
@@ -4,7 +4,7 @@ public class NewBranch1 {
     public static void main(String[] args) {
         String word= "Wooden spoon";
         System.out.println(word);
-        int new= 123;
-        System.out.println(new);
+        int newX= 123;
+        System.out.println(newX);
     }
 }
