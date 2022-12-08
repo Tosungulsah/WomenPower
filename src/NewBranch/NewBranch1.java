e2e4ef18176fffd6e6d8c2bde6341fcd311f5dfc
Tosungulsah <105287740+Tosungulsah@users.noreply.github.com>
2022-12-08 22:19:34 +0000

    Update NewBranch1.java

--- src/NewBranch/NewBranch1.java
+++ src/NewBranch/NewBranch1.java
@@ -4,5 +4,7 @@ public class NewBranch1 {
     public static void main(String[] args) {
         String word= "Wooden spoon";
         System.out.println(word);
+        int new= 123;
+        System.out.println(new);
     }
 }
